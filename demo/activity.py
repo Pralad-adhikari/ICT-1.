3ad76aadead198e9e6716265b4891569892d4b8e
Parlad Adhikari <08250278.sce@rub.edu.bt>
2026-03-20 16:12:31 +0600

    class activity

---
+++ demo/activity.py
@@ -0,0 +1,17 @@
+name = input("please enter your name:")
+print("kuzuzangpo",name)
+
+num1 = float(input("enter the first number:"))
+num2 = float(input("enter the second number:"))
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1*num2
+division_results = num1/num2
+exponentiation_result = num1**num2
+print(addition_result)
+print(subtraction_result)
+print(multiplication_result)
+print(division_results)
+print(exponentiation_result)
+
