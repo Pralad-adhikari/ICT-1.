16970e43bc9eee3bad28c580c59edb3e09194fc4
Parlad Adhikari <08250278.sce@rub.edu.bt>
2026-03-20 15:48:16 +0600

    learning logical codes

---
+++ demo/logical.py
@@ -0,0 +1,10 @@
+x = True
+y = False
+result_and = x and y
+print("logical AND:", result_and)
+
+result_or = x or y
+print("logical OR:", result_or)
+
+result_not = not x
+print("logical NOT:", result_not)
