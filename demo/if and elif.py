39e7143daaccaa923c21c782150149e585815696
Parlad Adhikari <08250278.sce@rub.edu.bt>
2026-03-25 10:11:17 +0600

    updated version

--- demo/if and elif.py
+++ demo/if and elif.py
@@ -2,6 +2,7 @@ Marks1 = float(input("Enter the marks of the first subject:"))
 Marks2 = float(input("Enter the marks of the second subject:"))
 Marks3 = float(input("Enter the marks of third third subject:"))
 average = ( Marks1 + Marks2 + Marks3)/ 3
+print("the average of 3 subjests are %.2f " %average )
 
 if(average >= 90 and Marks1 >= 50 and Marks2 >= 50 and Marsk3 >= 50): 
     print("Grade: A")
