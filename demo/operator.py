b4ca4e658a91c0ba4eb92b0a5386ccc3acc396e2
Parlad Adhikari <08250278.sce@rub.edu.bt>
2026-03-25 09:59:31 +0600

    learning operators

---
+++ demo/operator.py
@@ -0,0 +1,5 @@
+print("currently, we are having %s class" %"PRG101") #currently, we are having PRG101 class
+
+subject = "python"
+module_no = 101
+print("the module number of %s if %d" %(subject,module_no)) #Output: the module number of python is 101
