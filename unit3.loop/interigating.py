3f139c6e8a450d0574a95c146f1efe47e3f42915
Parlad Adhikari <08250278.sce@rub.edu.bt>
2026-04-15 08:44:31 +0600

    pushing interagating

---
+++ unit3.loop/interigating.py
@@ -0,0 +1,12 @@
+tup = ("john smith","jane doe","alice johnson")
+for x in tup:
+    print(x)
+
+set1 = {10,30,20}
+for x in set1:
+    print(x)
+
+Bookdetails = dict({"python programming":"joohn smith","python fundamentals":"alice johnson","python interview questions":"jane doe"})
+for keys in Bookdetails: #keys is a variable that takes the value of each key in the dictionary.
+    print(keys,Bookdetails[keys])
+
